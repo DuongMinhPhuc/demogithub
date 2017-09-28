5feb84bfe4e49cfcfa3e13798f03a3b5c8830ae2
duong minh phuc <phucduong2397@gmail.com>
2017-09-28 17:32:38 +0700

    tao file demogit hub

---
+++ app/src/main/java/phucduongcom/demofilebase/model/DemoGitHub.java
@@ -0,0 +1,12 @@
+package phucduongcom.demofilebase.model;
+
+/**
+ * Created by phucd on 9/28/2017.
+ */
+
+public class DemoGitHub {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
